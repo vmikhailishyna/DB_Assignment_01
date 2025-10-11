8ef042eafe0c25b891790deff02d25d354bebc10
vmikhailishyna <vmikhailishyna@kse.org.ua>
2025-10-11 11:22:37 +0300

    Update assignment_01_queries.sql

--- assignment_01_queries.sql
+++ assignment_01_queries.sql
@@ -1,61 +1,36 @@
 use Assignment_01;
 
-select b.Name as BodyguardName,
-		b.Surname as BodyguardSurname,
-		s.TypeOfService,
-		c.Name as ClientName,
-		o.OrderID,
-		p.Cost
-from Orders o
-join Clients c on o.ClientID = c.ClientID
-join Bodyguard b on o.BodyguardID = b.BodyguardID
-join Service s on o.ServiceID = s.ServiceID
-join Price p on s.ServiceID = p.ServiceID
-where o.OrderDate > '2025-03-01'
-order by p.Cost desc
-limit 5;
-
-
-SELECT b.Name as BodyguardName,
-SUM(p.Cost) AS Sallary
-FROM Orders o
-join Bodyguard b on o.BodyguardID = b.BodyguardID
-join Service s on o.ServiceID = s.ServiceID
-join Price p on s.ServiceID = p.ServiceID
-GROUP BY b.BodyguardID 
-having Sallary > 300
-order by Sallary desc;
-
-with count_cte as(
-select b.Name as BodyguardName,
-b.Surname as BodyguardSurname,
-Count(o.BodyguardID) as total_orders,
-SUM(p.Cost) AS total_income
-from Orders o
-join Bodyguard b on o.BodyguardID = b.BodyguardID
-JOIN Service s ON o.ServiceID = s.ServiceID
+WITH count_cte AS (
+    SELECT 
+        b.BodyguardID,
+        b.Name AS BodyguardName,
+        b.Surname AS BodyguardSurname,
+        COUNT(o.OrderID) AS total_orders,
+        SUM(p.Cost) AS total_income
+    FROM Orders o
+    JOIN Bodyguard b ON o.BodyguardID = b.BodyguardID
+    JOIN Service s ON o.ServiceID = s.ServiceID
     JOIN Price p ON s.ServiceID = p.ServiceID
     GROUP BY b.BodyguardID, b.Name, b.Surname
+    HAVING SUM(p.Cost) > 300
 )
-select cte.BodyguardName,
-cte.BodyguardSurname,
-cte.total_orders,
-cte.total_income
-from count_cte cte
-order by cte.total_income DESC;
-
-select o.OrderID,
-s.TypeOfService,
-p.Cost
-from Orders o
-join Service s on o.ServiceID = s.ServiceID
-join Price p on s.ServiceID = p.ServiceID
-where p.Cost >(
-select avg(Cost)
-from Price)
-order by p.Cost desc;
-
-
-select Name from Bodyguard
-union 
-select Name from clients;
+SELECT 
+    cte.BodyguardName,
+    cte.BodyguardSurname,
+    c.Name AS ClientName,
+    s.TypeOfService,
+    p.Cost AS ServiceCost,
+    cte.total_orders,
+    cte.total_income
+FROM Orders o
+JOIN Clients c ON o.ClientID = c.ClientID
+JOIN Bodyguard b ON o.BodyguardID = b.BodyguardID
+JOIN Service s ON o.ServiceID = s.ServiceID
+JOIN Price p ON s.ServiceID = p.ServiceID
+JOIN count_cte AS cte ON b.BodyguardID = cte.BodyguardID
+WHERE 
+    o.OrderDate > '2025-03-01'                        
+    AND p.Cost > (SELECT AVG(Cost) FROM Price)
+ORDER BY 
+    cte.total_income DESC 
+LIMIT 5;
